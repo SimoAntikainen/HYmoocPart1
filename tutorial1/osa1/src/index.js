dae57ea582d8161ed7b779bcd479706e73f127ad
Simo Antikainen <simo.antikainen@aalto.fi>
2018-06-08 23:09:42 +0300

    end of day commit

--- tutorial1/osa1/src/index.js
+++ tutorial1/osa1/src/index.js
@@ -4,12 +4,28 @@ import ReactDOM from 'react-dom';
 //import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
 
-const Hello = (props) => {
+/**const Hello = (props) => {
     return (
         <div>
             <p>Hello {props.name} you are {props.age} years old</p>
         </div>
     )
+}**/
+
+class Hello extends React.Component {
+  render() {
+    const {name, age} = this.props
+    const bornYear = () => new Date().getFullYear() - age
+
+    return (
+      <div>
+        <p>
+          Hello {name}, you are {age} years old <br />
+          So you were probably born {bornYear()}
+        </p>
+      </div>
+    )
+  }
 }
 
 const Footer = () => {
@@ -18,16 +34,10 @@ const Footer = () => {
     )
   }
 
+  /** 
 const App = () => {
 
-    /**const now = new Date()
-    const a = 10
-    const b = 20
-    return (
-      <div>
-        <p>Hello world, it is {now.toString()}</p>
-        <p>{a} plus {b} is {a+b}</p>
-      </div>**/
+    
       const nimi= 'Pekka'
       const ika= 10
 
@@ -44,5 +54,83 @@ const App = () => {
         
       )}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+      */
+
+    
+    /**  const App = (props) => {
+      const {counter} = props
+      return (
+        <div>{counter.value}</div>
+      )
+    }
+    
+    const counter = {
+      value: 1
+    }
+
+    const renderoi = () => {
+      ReactDOM.render(
+        <App counter={counter} />,
+        document.getElementById('root')
+      )
+    }
+    
+    setInterval(() => {
+      renderoi()
+      counter.value += 1;
+    }, 1000)**/
+
+//ReactDOM.render(<App counter={counter}/>, document.getElementById('root'));
+    class App extends React.Component {
+      constructor(props) {
+        super(props)
+        this.state = {
+          counter: 1
+        }
+        this.kasvataYhdella = this.kasvataYhdella.bind(this)
+    this.nollaa = this.nollaa.bind(this)
+
+        setInterval(() => {
+          this.setState({ counter: this.state.counter + 1 })
+        }, 1000)
+
+      }
+      kasvataYhdella() {
+        this.setState({ counter: this.state.counter + 1 })
+      }
+    
+      nollaa() {
+        this.setState({ counter: 0 })
+      }
+    
+      render() {
+        console.log('renderöidään', this.state.counter)
+
+        function handleClick() {
+          console.log('Button was pressed.');
+          //Does not work
+          this.setState({ counter: this.state.counter + 1 })
+        }
+
+        return (
+          <div>{this.state.counter}
+            <button onClick={this.kasvataYhdella}>
+              plus
+            </button>
+            <button onClick={this.nollaa}>
+            zero
+            </button>
+          </div>
+        )
+      }
+    }
+    
+    ReactDOM.render(
+      <App />,
+      document.getElementById('root')
+    )
+
+
+
+
 //registerServiceWorker();
