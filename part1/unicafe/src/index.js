c4d7eb4876fe52936f93d2d30bda302c6cdeed85
Simo Antikainen <simo.antikainen@aalto.fi>
2018-06-10 21:20:00 +0300

    unicafe part1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,13 +3,34 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hyva: 0,
+      neutraali: 0,
+      huono: 0
+    }
+  }
   
 
   render() {
     return (
     <div>
       <div>
-      <h1>Anna palautetta</h1>
+        <h1>Anna palautetta</h1>
+      </div>
+      <div>
+        <button onClick={() => this.setState({hyva: this.state.hyva + 1})}>hyvä</button>
+        <button onClick={() => this.setState({neutraali: this.state.neutraali + 1})}>neutraali</button>
+        <button onClick={() => this.setState({huono: this.state.huono + 1})}>huono</button>
+      </div>
+      <div>
+        <h1>Statistiikka</h1>
+      </div>
+      <div>
+        <p>Hyvä {this.state.hyva}</p>
+        <p>Neutraali {this.state.neutraali}</p>
+        <p>Huono {this.state.huono}</p>
       </div>
     </div>
     )
