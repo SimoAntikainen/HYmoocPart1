aee8ab9193539771558295116213f22e9b5548e4
Simo Antikainen <simo.antikainen@aalto.fi>
2018-06-10 20:46:37 +0300

    unicafe initial commit

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+  
+
+  render() {
+    return (
+    <div>
+      <div>
+      <h1>Anna palautetta</h1>
+      </div>
+    </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
