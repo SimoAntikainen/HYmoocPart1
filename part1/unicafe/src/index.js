04c27259d11a7aada9c2d3f9e2a3c156440d86a3
Simo Antikainen <simo.antikainen@aalto.fi>
2018-06-10 22:04:40 +0300

    part 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -8,8 +8,35 @@ class App extends React.Component {
     this.state = {
       hyva: 0,
       neutraali: 0,
-      huono: 0
+      huono: 0,
+      keskiarvo: 0,
+      positiivisia: 0
     }
+  }
+
+  lisaaHyva = () => {
+    this.setState({hyva: this.state.hyva + 1})
+    this.laskeKeskiarvo()
+  }
+  lisaaNeutraali = () => {
+    this.setState({neutraali: this.state.neutraali + 1})
+    this.laskeKeskiarvo()
+  }
+  lisaaHuono = () => {
+    this.setState({huono: this.state.huono + 1})
+    this.laskeKeskiarvo()
+  }
+
+
+  laskeKeskiarvo = () => {
+    console.log('Here');
+    const laskettuArvo = (this.state.hyva * 1 + this.state.huono * -1) 
+    / (this.state.hyva + this.state.neutraali +  this.state.huono)
+    this.setState({keskiarvo: laskettuArvo})  
+  }
+
+  laskePositiiviset = () => {
+    
   }
   
 
@@ -20,9 +47,9 @@ class App extends React.Component {
         <h1>Anna palautetta</h1>
       </div>
       <div>
-        <button onClick={() => this.setState({hyva: this.state.hyva + 1})}>hyvä</button>
-        <button onClick={() => this.setState({neutraali: this.state.neutraali + 1})}>neutraali</button>
-        <button onClick={() => this.setState({huono: this.state.huono + 1})}>huono</button>
+        <button onClick={this.lisaaHyva}>hyvä</button>
+        <button onClick={this.lisaaNeutraali}>neutraali</button>
+        <button onClick={this.lisaaHuono}>huono</button>
       </div>
       <div>
         <h1>Statistiikka</h1>
@@ -31,6 +58,8 @@ class App extends React.Component {
         <p>Hyvä {this.state.hyva}</p>
         <p>Neutraali {this.state.neutraali}</p>
         <p>Huono {this.state.huono}</p>
+        <p>keskiarvo {this.state.keskiarvo}</p>
+        <p>positiivisia {this.state.positiivisia}</p>
       </div>
     </div>
     )
